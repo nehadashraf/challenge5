0f7a2d6cf35fc339c3ea6353e56bb1eb02c5a411
nehadashraf <nehadashraf402@gmail.com>
2022-11-26 21:35:13 +0200

    first commit

---
+++ assets/JS/app.js
@@ -0,0 +1,15 @@
+jQuery("#responsive_headline").fitText();
+wow = new WOW(
+    {
+    boxClass:     'animate__animated',      // default
+  }
+  )
+  wow.init();
+$(function() {  
+    $("body").niceScroll(
+      {
+        cursorcolor: "black",
+        cursorwidth: "5px"
+      }
+    );
+});
